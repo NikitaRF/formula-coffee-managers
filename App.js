d1ebab6d5ddd9269bf24abbd37788134b052f2b9
nikitabeloshapka <existing@live.ru>
2021-06-03 15:50:15 +0500

    Mac

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import AppLoading from "expo-app-loading";
 import { StyleSheet, Text, View } from 'react-native';
+import {THEME} from "./src/theme";
+
+import {bootstrap} from "./src/bootstrap";
 
 export default function App() {
+    const [isReady, setIsReady] = useState(false)
+
+    if(!isReady){
+        return (
+            <AppLoading
+            startAsync={bootstrap}
+            onFinish={() => setIsReady(true)}
+            onError={(e) => console.log(e)}
+            />
+        )
+    }
+
+
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.center}>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    text: {
+        fontFamily: 'open-bold',
+        color: THEME.MAIN_COLOR
+    },
+    center: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
