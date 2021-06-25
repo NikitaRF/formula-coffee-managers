9cad3adcb6289edfbcb627480c7c629827609bb3
nikitabeloshapka <existing@live.ru>
2021-06-25 16:26:41 +0500

    Mac

--- src/screens/ContactsScreen.js
+++ src/screens/ContactsScreen.js
@@ -1,15 +1,37 @@
 import React from "react";
-import {Platform, StyleSheet, Text, View} from "react-native";
-import {StatusBar} from "expo-status-bar";
+import {Linking, Platform, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+
 
 import {THEME} from "../theme";
 
 export const ContactsScreen = () => {
 
+    const dialCall = () => {
+        let phoneNumber = '+79224280035';
+        Linking.openURL(`tel:${phoneNumber}`);
+    };
+
     return (
         <View style={styles.center}>
-            <Text style={styles.text}>Контакты</Text>
-            <StatusBar style="auto" />
+            <View style={styles.infoBlock}>
+                <Text style={styles.textTitle}>Адрес</Text>
+                <Text style={styles.textBody}>628406, Сургут, Югорский тракт, дом 4, офис 5</Text>
+            </View>
+
+                <View style={{...styles.infoBlock, flexDirection: 'row', alignItems: 'center',}}>
+                    <Text style={styles.textTitle}>Телефон</Text>
+                    <TouchableOpacity onPress={dialCall}>
+                        <Text style={{...styles.textBody, marginTop: 0,}}>+7 922 428 00 35</Text>
+                    </TouchableOpacity>
+                </View>
+
+            <View style={styles.infoBlock}>
+                <Text style={styles.textTitle}>Часы работы</Text>
+                <Text style={styles.textBody}>Будние дни</Text>
+            </View>
+
+
+
         </View>
     )
 }
@@ -17,14 +39,38 @@ export const ContactsScreen = () => {
 
 
 const styles = StyleSheet.create({
-    text: {
+    center: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 20,
+    },
+    infoBlock: {
+        marginBottom: 20,
+        width: '85%',
+        padding: 20,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        shadowColor: '#000',
+        shadowRadius: 2,
+        shadowOpacity: 0.3,
+        shadowOffset: { width: 2, height: 2 },
+        elevation: 8,
+        backgroundColor: '#fff',
+        borderRadius: 10
+    },
+    textTitle: {
         fontFamily: 'open-bold',
         color: THEME.COLOR_MAIN_DARK
     },
-    center: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+    textBody: {
+        marginTop: 15,
+        fontFamily: 'open-regular',
+        color: THEME.COLOR_MAIN_DARK
+    },
+
+
 })
 
