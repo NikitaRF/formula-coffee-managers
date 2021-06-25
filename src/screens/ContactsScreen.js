2cd6552f2eb5a06e90a4a2f9956204ace50770d3
nikitabeloshapka <existing@live.ru>
2021-06-25 17:13:07 +0500

    Mac

--- src/screens/ContactsScreen.js
+++ src/screens/ContactsScreen.js
@@ -7,31 +7,66 @@ import {THEME} from "../theme";
 export const ContactsScreen = () => {
 
     const dialCall = () => {
-        let phoneNumber = '+79224280035';
+        const phoneNumber = '+79224280035';
         Linking.openURL(`tel:${phoneNumber}`);
+    };
+
+    const sendMail = () => {
+        const email = 'info@formula-coffee.ru';
+        Linking.openURL(`mailto:${email}`);
     };
 
     return (
         <View style={styles.center}>
             <View style={styles.infoBlock}>
                 <Text style={styles.textTitle}>Адрес</Text>
-                <Text style={styles.textBody}>628406, Сургут, Югорский тракт, дом 4, офис 5</Text>
+                <Text style={styles.textBody}>628406, Сургут</Text>
+                <Text style={styles.textBody}>Югорский тракт 4, офис 5</Text>
             </View>
 
-                <View style={{...styles.infoBlock, flexDirection: 'row', alignItems: 'center',}}>
-                    <Text style={styles.textTitle}>Телефон</Text>
-                    <TouchableOpacity onPress={dialCall}>
-                        <Text style={{...styles.textBody, marginTop: 0,}}>+7 922 428 00 35</Text>
-                    </TouchableOpacity>
-                </View>
+            <View style={{...styles.infoBlock, flexDirection: 'row', alignItems: 'center',}}>
+                <Text style={styles.textTitle}>Телефон</Text>
+                <TouchableOpacity onPress={dialCall}>
+                    <Text style={{...styles.textBody, marginTop: 0,}}>+7 922 428 00 35</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={{...styles.infoBlock, flexDirection: 'row', alignItems: 'center',}}>
+                <Text style={styles.textTitle}>Email</Text>
+                <TouchableOpacity onPress={sendMail}>
+                    <Text style={{...styles.textBody, marginTop: 0,}}>info@formula-coffee.ru</Text>
+                </TouchableOpacity>
+            </View>
 
             <View style={styles.infoBlock}>
                 <Text style={styles.textTitle}>Часы работы</Text>
-                <Text style={styles.textBody}>Будние дни</Text>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
+                    <Text style={styles.textBody}>Будние дни</Text>
+                    <Text style={styles.textBody}>8:30 - 23:00</Text>
+                </View>
+                <View style={{flexDirection: 'row', justifyContent: 'space-around', width: '100%', marginBottom: 20}}>
+                    <View style={{flexDirection: 'column'}}>
+                        <Text style={styles.textBody}>Завтраки</Text>
+                        <Text style={styles.textBody}>9:00 - 16:00</Text>
+                    </View>
+                    <View style={{flexDirection: 'column'}}>
+                        <Text style={styles.textBody}>Бизнес-ланч</Text>
+                        <Text style={styles.textBody}>12:00 - 17:00</Text>
+                    </View>
+                </View>
+
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
+                    <Text style={styles.textBody}>Выходные дни</Text>
+                    <Text style={styles.textBody}>7:30 - 23:00</Text>
+                </View>
+
+                <View style={{flexDirection: 'row', justifyContent: 'space-around', width: '100%'}}>
+                    <View style={{flexDirection: 'column'}}>
+                        <Text style={styles.textBody}>Завтраки</Text>
+                        <Text style={styles.textBody}>7:30 - 16:00</Text>
+                    </View>
+                </View>
             </View>
-
-
-
         </View>
     )
 }
