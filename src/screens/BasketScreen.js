361d7d3705fbaf40b4cb45201d2339aa79c7f36c
nikitabeloshapka <existing@live.ru>
2021-07-19 12:32:49 +0500

    Mac

--- src/screens/BasketScreen.js
+++ src/screens/BasketScreen.js
@@ -1,11 +1,15 @@
 import React, {useEffect, useState} from "react";
-import {Button, FlatList, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Button, FlatList, Modal, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import {useSelector} from "react-redux";
 import {BasketItem} from "../components/BasketItem";
 import {THEME} from "../theme";
+import {AntDesign} from "@expo/vector-icons";
+import {addToBasket} from "../store/actions/addToBasket";
+import {deleteItemFromBasket} from "../store/actions/deleteItemFromBasket";
 
-export const BasketScreen = () => {
+export const BasketScreen = ({navigation}) => {
     const itemsBasket = useSelector(state => state.menu.basket)
+    const [modal, setModal] = useState(false)
 
     // Начало Счетчик товаров в Корзине
     const [totalCount, setTotalCount] = useState()
@@ -40,11 +44,98 @@ export const BasketScreen = () => {
     // Конец Счетчик товаров в Корзине
 
     const getOrder = () => {
-
+        setModal(true)
     }
 
 
     console.log('Баскет скрин:', itemsBasket)
+
+    const [state, setState] = useState({
+        isLoading: false
+    })
+    const userData = useSelector(state => state.user.userInfo)
+
+    const updateInputVal = (val, prop) => {
+        setState({
+            ...state,
+            [prop]: val
+        });
+    }
+
+    const [countOfPerson, setCountOfPerson] = useState(1)
+    const plusPerson = () => {
+        setCountOfPerson(countOfPerson + 1)
+        if (countOfPerson == 12) {
+            setCountOfPerson(12)
+        }
+    }
+    const minusPerson = () => {
+        setCountOfPerson(countOfPerson - 1)
+        if (countOfPerson == 1) {
+            setCountOfPerson(1)
+        }
+    }
+
+    if (modal) {
+        return (
+        <Modal visible={modal} animationType='slide' transparent={false}>
+            <ScrollView contentContainerStyle={styles.modalWrap}>
+                <View><Text>Адрес</Text></View>
+                <TextInput />
+                <View><Text>Комментарий</Text></View>
+                <View><Text>Телефон</Text></View>
+                <View style={styles.inputWrap}>
+                    <TextInput
+                        color={THEME.COLOR_MAIN_DARK}
+                        autoCorrect={false}
+                        value={state.phone}
+                        placeholder='Телефон'
+                        style={styles.input}
+                        maxLength={11}
+                        defaultValue={userData.phone}
+                        keyboardType='phone-pad'
+                        onChangeText={(val) => updateInputVal(val, 'phone')}
+                    />
+                    <AntDesign
+                        name="checksquare"
+                        size={24}
+                        style={styles.allowIcon}
+                        color={THEME.COLOR_MAIN_DARK}
+                        onPress={() => saveChanges('phone', state.phone)}
+                    />
+                </View>
+
+
+                <View style={styles.modalPersonWrap}>
+                    <Text style={styles.modalPersonTitle}>Количество персон</Text>
+                    <View>
+                        <View style={styles.buttonCount}>
+                            <AntDesign
+                                name="minuscircle"
+                                size={24}
+                                color={THEME.COLOR_MAIN_DARK}
+                                onPress={() => minusPerson()}
+                            />
+                            <View style={styles.wrapCount}>
+                                <Text style={styles.textCount}>{countOfPerson}</Text>
+                            </View>
+                            <AntDesign
+                                name="pluscircle"
+                                size={24}
+                                color={THEME.COLOR_MAIN_DARK}
+                                onPress={() => plusPerson()}
+                            />
+                        </View>
+                    </View>
+                </View>
+                <View><Text>Итого {totalPrice} руб</Text></View>
+                <View style={styles.modalButtons}>
+                    <Button title='Отменить' onPress={() => setModal(false)} color='red' />
+                    <Button title='Заказать' color={THEME.COLOR_MAIN_DARK}/>
+                </View>
+            </ScrollView>
+        </Modal>
+    )}
 
     return (
         <View style={styles.center}>
@@ -70,7 +161,6 @@ export const BasketScreen = () => {
                             </Text>
                         </View>
                     </View>
-
                 </TouchableOpacity>
             }
 
@@ -105,5 +195,47 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: THEME.COLOR_MAIN_DARK,
         fontFamily: 'open-bold',
+    },
+    modalWrap: {
+        marginTop: 50,
+        marginHorizontal: 25,
+    },
+    modalPersonWrap: {
+        flexDirection: 'row',
+    },
+    modalPersonTitle: {
+        color: THEME.COLOR_MAIN_DARK,
+        fontFamily: 'open-regular',
+        marginRight: 15,
+        fontSize: 18,
+    },
+    modalButtons: {
+        width: '100%',
+        marginTop: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    buttonCount: {
+        flexDirection: 'row',
+    },
+    wrapCount: {
+        paddingHorizontal: 15,
+    },
+    textCount: {
+        fontFamily: 'open-bold',
+        fontSize: 18,
+        color: THEME.COLOR_MAIN_DARK,
+    },
+    inputWrap: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    input: {
+        borderStyle: 'solid',
+        borderColor: THEME.COLOR_MAIN_DARK,
+        borderBottomWidth: 1,
+        width: '100%',
+        height: 45,
+        marginTop: 20,
     },
 })
