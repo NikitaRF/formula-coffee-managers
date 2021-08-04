4fcae69c630d746a246c0ef4730e5b8bfe6d9b56
Nikita <existing@live.ru>
2021-08-04 22:15:22 +0500

    Home

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -3,8 +3,21 @@ import {Image, StyleSheet, Text, View} from "react-native";
 import {StatusBar} from "expo-status-bar";
 
 import {THEME} from "../theme";
+import firebase from "firebase";
 
 export const MainScreen = () => {
+
+    // выборка из БД, пример
+    // const db = firebase.firestore();
+    // db.collection('users').get().then((querySnapshot) => {
+    //     querySnapshot.forEach((doc) => {
+    //         // doc.data() is never undefined for query doc snapshots
+    //         let res = doc.data().historyOfOrder.filter(function (el) {
+    //             return el.status === 'В обработке'
+    //         })
+    //         console.log(res)
+    //     });
+    // })
 
     return (
         <View style={styles.container}>
