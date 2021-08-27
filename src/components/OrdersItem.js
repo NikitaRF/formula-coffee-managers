1c8d25c1903213fd055463c1b70e1311e1624e9b
nikitabeloshapka <existing@live.ru>
2021-08-27 09:17:11 +0500

    Mac

--- src/components/OrdersItem.js
+++ src/components/OrdersItem.js
@@ -150,7 +150,7 @@ const styles = StyleSheet.create({
     preloader: {
         left: 0,
         right: 0,
-        top: 50,
+        top: 0,
         bottom: 0,
         backgroundColor: '#fff'
     },
